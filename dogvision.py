4a1c7e1a339d46ec1eb8491290ac264508cfad3c
Abhik <abhikdas36@gmail.com>
2021-03-26 21:23:53 +0530

    adding new features to the website

---
+++ dogvision.py
@@ -0,0 +1,81 @@
+import json
+import requests
+import numpy as np
+import tensorflow_datasets as tfds
+
+import tensorflow as tf
+
+breeds = ['affenpinscher', 'afghan_hound', 'african_hunting_dog', 'airedale',
+          'american_staffordshire_terrier', 'appenzeller',
+          'australian_terrier', 'basenji', 'basset', 'beagle',
+          'bedlington_terrier', 'bernese_mountain_dog',
+          'black-and-tan_coonhound', 'blenheim_spaniel', 'bloodhound',
+          'bluetick', 'border_collie', 'border_terrier', 'borzoi',
+          'boston_bull', 'bouvier_des_flandres', 'boxer',
+          'brabancon_griffon', 'briard', 'brittany_spaniel', 'bull_mastiff',
+          'cairn', 'cardigan', 'chesapeake_bay_retriever', 'chihuahua',
+          'chow', 'clumber', 'cocker_spaniel', 'collie',
+          'curly-coated_retriever', 'dandie_dinmont', 'dhole', 'dingo',
+          'doberman', 'english_foxhound', 'english_setter',
+          'english_springer', 'entlebucher', 'eskimo_dog',
+          'flat-coated_retriever', 'french_bulldog', 'german_shepherd',
+          'german_short-haired_pointer', 'giant_schnauzer',
+          'golden_retriever', 'gordon_setter', 'great_dane',
+          'great_pyrenees', 'greater_swiss_mountain_dog', 'groenendael',
+          'ibizan_hound', 'irish_setter', 'irish_terrier',
+          'irish_water_spaniel', 'irish_wolfhound', 'italian_greyhound',
+          'japanese_spaniel', 'keeshond', 'kelpie', 'kerry_blue_terrier',
+          'komondor', 'kuvasz', 'labrador_retriever', 'lakeland_terrier',
+          'leonberg', 'lhasa', 'malamute', 'malinois', 'maltese_dog',
+          'mexican_hairless', 'miniature_pinscher', 'miniature_poodle',
+          'miniature_schnauzer', 'newfoundland', 'norfolk_terrier',
+          'norwegian_elkhound', 'norwich_terrier', 'old_english_sheepdog',
+          'otterhound', 'papillon', 'pekinese', 'pembroke', 'pomeranian',
+          'pug', 'redbone', 'rhodesian_ridgeback', 'rottweiler',
+          'saint_bernard', 'saluki', 'samoyed', 'schipperke',
+          'scotch_terrier', 'scottish_deerhound', 'sealyham_terrier',
+          'shetland_sheepdog', 'shih-tzu', 'siberian_husky', 'silky_terrier',
+          'soft-coated_wheaten_terrier', 'staffordshire_bullterrier',
+          'standard_poodle', 'standard_schnauzer', 'sussex_spaniel',
+          'tibetan_mastiff', 'tibetan_terrier', 'toy_poodle', 'toy_terrier',
+          'vizsla', 'walker_hound', 'weimaraner', 'welsh_springer_spaniel',
+          'west_highland_white_terrier', 'whippet',
+          'wire-haired_fox_terrier', 'yorkshire_terrier']
+
+
+def get_image(image):
+    tensor = tf.io.decode_jpeg(image, channels=3)
+    color_channel = tf.image.convert_image_dtype(tensor, tf.float32)
+    resize = tf.image.resize(color_channel, (224, 224))
+
+    return resize
+
+
+def create_batch(data):
+    dataset = tf.data.Dataset.from_tensors(tf.constant(data))
+    data_batch = dataset.map(get_image).batch(1)
+    convert_to_numpy = tfds.as_numpy(data_batch)
+
+    for i in convert_to_numpy:
+        return tf.constant(i)
+
+
+def make_predictions(batch_data):
+    headers = {"content-type": "application/json"}
+    url = 'https://boiling-sea-15225.herokuapp.com'
+    full_url = f"{url}/v1/models/full-data-resnet50v2/versions/1:predict"
+
+    data = json.dumps({"signature_name": "serving_default",
+                       "instances": create_batch(batch_data).numpy().tolist()})
+
+    response = requests.post(full_url, data=data, headers=headers)
+    predictions = json.loads(response.text)['predictions']
+
+    return get_breed(predictions[0])
+
+
+def get_breed(prediction):
+    max_index = breeds[np.argmax(prediction)]
+    score = np.max(prediction)
+
+    return max_index, score
--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, jsonify
 from pymongo import MongoClient
+
 import HN_Scraping
+import dogvision
 from HN_Scraping import hn
 
 app = Flask(__name__)
 
-cluster = MongoClient('mongodb+srv://admin:admin@cluster0.xzjm3.mongodb.net/portfolio?retryWrites=true&w=majority')  # mongodb connection string
+cluster = MongoClient(
+    'mongodb+srv://admin:admin@cluster0.xzjm3.mongodb.net/portfolio?retryWrites=true&w=majority')  # mongodb connection string
 db = cluster.get_database('portfolio')
 collection = db.messages
 
@@ -36,6 +39,7 @@ def submit_form():
             'message': request.form['message']
         }
         collection.insert_one(data)
+
         return render_template('/thankyou.html')
     else:
         return 'something went wrong. Try again!!'
@@ -47,4 +51,27 @@ def get_input():
     inp = request.form['input']
     HN_Scraping.import_links(inp)
     result = HN_Scraping.sorted_hn(hn_list=hn)
+
     return render_template('scrape.html', result=result)
+
+
+# get input data and predict on that
+@app.route('/make_prediction', methods=['GET', 'POST'])
+def make_prediction():
+    breed = None
+    letters = ['a', 'e', 'i', 'o', 'u']
+    prediction_score = None
+
+    if request.method == 'POST':
+        if request.files:
+            inp = request.files['image'].read()
+            prediction = dogvision.make_predictions(inp)
+            breed = prediction[0]
+
+            if breed[0] in letters:
+                breed = f"It's an {breed}"
+
+            breed = f"It's a {breed}"
+            prediction_score = f"I'm {int(prediction[1] * 100)} % sure of it"
+
+    return render_template('dogbreed.html', breed=breed, prediction_score=prediction_score)
